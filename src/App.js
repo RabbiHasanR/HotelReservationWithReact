0222cb5b1b2505e06d9688b33631abdc76d00f61
Rabbi50 <jasrabbi50@gmail.com>
2020-09-08 19:34:09 +0600

    create pages component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+
 
 function App() {
   return (
-    <>Hello from app!</>
+    <>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+
+    </>
   );
 }
 
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                Hello from single room page!
+            </div>
+        )
+    }
+}
