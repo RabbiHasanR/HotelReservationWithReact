e1e458a1d3a5978a3fa8c86646dba27a665055e3
Rabbi50 <jasrabbi50@gmail.com>
2020-09-08 19:57:33 +0600

    set up routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
 
+
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
-
+    <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms/' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route component={Error} />
+    </Switch>
     </>
   );
 }
