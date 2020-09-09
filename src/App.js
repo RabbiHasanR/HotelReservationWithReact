4e1e707a1a5335096a817e60049164404d5ea085
Rabbi50 <jasrabbi50@gmail.com>
2020-09-09 20:18:11 +0600

    create navbar component with toggle

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import Navbar from './components/Navbar'
 
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/rooms/' component={Rooms} />
