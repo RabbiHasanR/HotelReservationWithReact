7d569b33b102ae179a1b0edb28c028ee393273e0
Rabbi50 <jasrabbi50@gmail.com>
2020-09-09 21:03:50 +0600

    create banner components for home,rooms and error pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <Hero />
+        <Hero>
+            <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
+                <Link to='/rooms' className='btn-primary'>
+                Our rooms
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 export default function Rooms() {
     return (
-        <Hero hero='roomsHero'/>
+        <Hero hero='roomsHero'>
+            <Banner title='our rooms'>
+                <Link to='/' className='btn-primary'>
+                    return home
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
