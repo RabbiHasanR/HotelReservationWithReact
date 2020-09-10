c14743a0847ce360f83171ae27831d1fa47a1321
Rabbi50 <jasrabbi50@gmail.com>
2020-09-10 14:44:28 +0600

    complete featuredRooms component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import { RoomContext} from '../context'
+import Loading from './Loading'
+import Room from './Room'
+import Title from './Title'
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-        const {featuredRooms:rooms}=this.context
-        console.log(rooms)
+        let {loading,featuredRooms:rooms}=this.context
+
+        rooms = rooms.map(room=>{
+            return <Room key={room.id} room={room}/>
+        })
         return (
-            <div>
-                Hello from featured rooms!
-            </div>
+            <section className='featured-rooms'>
+                <Title title="featured rooms"/>
+                <div className='featured-rooms-center'>
+                    {loading?<Loading />:rooms}
+                </div>
+            </section>
         )
     }
 }
