d2e4f2371c84ffb504685bb31df3b5a0d8e11ea8
Rabbi50 <jasrabbi50@gmail.com>
2020-09-10 14:26:03 +0600

    load and format data from data.jsfor context api

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,10 +4,11 @@ import { RoomContext} from '../context'
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-        const {greeting,name}=this.context
+        const {featuredRooms:rooms}=this.context
+        console.log(rooms)
         return (
             <div>
-                Hello from featured rooms!{greeting} {name}
+                Hello from featured rooms!
             </div>
         )
     }
--- src/context.js
+++ src/context.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react'
 
+import items from './data'
+
 const RoomContext=React.createContext()
 
 class RoomProvider extends Component {
     state={
-        greeting:'Hello',
-        name:'Rabbi'
+       rooms:[],
+       sortedRooms:[],
+       featuredRooms:[],
+       loading:true
     }
+
+    componentDidMount(){
+        //get data
+        let rooms=this.formatData(items)
+        let featuredRooms=rooms.filter(room=>room.featured===true)
+        this.setState({
+            rooms,
+            sortedRooms:rooms,
+            featuredRooms,
+            loading:false
+        })
+    }
+
+    formatData(items){
+        let tempItems = items.map(item=>{
+            let id=item.sys.id
+            let images=item.fields.images.map(image=>image.fields.file.url)
+            let room={...item.fields,images,id}
+
+            return room
+        })
+        return tempItems
+    }
+
+
     render() {
         return (
             <RoomContext.Provider value={{...this.state}}>
