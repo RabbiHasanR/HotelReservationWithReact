45f1cc38fad7fe6cf09938fd2b7b6cf5adaae226
Rabbi50 <jasrabbi50@gmail.com>
2020-09-10 19:38:12 +0600

    get room info for every sinlge room in single room page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import defaultImg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {RoomContext} from '../context'
+import {Link} from 'react-router-dom'
 
 export default class SingleRoom extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+            slug:this.props.match.params.slug,
+            defaultImg
+        }
+    }
+    static contextType=RoomContext
+    // componentDidMount(){
+
+    // }
     render() {
-        return (
-            <div>
-                Hello from single room page!
+        const {getRoom}=this.context
+        const room=getRoom(this.state.slug)
+        if(!room){
+            return(
+                <div className='error'>
+                <h3>no such room could be found</h3>
+                <Link to='/rooms' className='btn-primary'>
+                    back to rooms
+                </Link>
             </div>
+            )
+        }
+        const {name,description,capacity,size,price,extras,breakfast,pets,images}=room
+        return (
+            <Hero hero='roomsHero'>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary'>
+                        back to rooms
+                    </Link>
+                </Banner>
+            </Hero>
         )
     }
 }
