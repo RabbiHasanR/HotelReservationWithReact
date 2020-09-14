1d28024de3654d3cf84eeff69cdbde9eb911345f
Rabbi50 <jasrabbi50@gmail.com>
2020-09-14 21:31:10 +0600

    filter complete by price,size,breakfast and pets

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -57,6 +57,39 @@ export default function RoomsFilter({rooms}) {
                 </div>
                 {/*end Guests*/}
 
+                {/*room price*/}
+                <div className='form-group'>
+                    <label htmlFor='price'>room price ${price}</label>
+                    <input type='range' name='price' min={minPrice}
+                    max={maxPrice} id='price' value={price} onChange={handleChange} className='form-control'/>
+                </div>
+                {/*end room price*/}
+
+                {/*size*/}
+                <div className='form-group'>
+                    <label htmlFor='size'>room size</label>
+                    <div className='size-inputs'>
+                        <input type='number' name='minSize' value={minSize} id='size' onChange={handleChange} className='size-input'/>
+                        <input type='number' name='maxSize' value={maxSize} id='size' onChange={handleChange} className='size-input'/>
+                    </div>
+                </div>
+                {/*end size*/}
+
+                {/*extras*/}
+                <div className='form-group'>
+                    <div className='single-extra'>
+                        <input type='checkbox' name='breakfast' checked={breakfast} id='breakfast' onChange={handleChange}/>
+                        <label htmlFor='breakfast'>Breakfast</label>
+                    </div>
+                    <div className='single-extra'>
+                        <input type='checkbox' name='pets' checked={pets} id='pets' onChange={handleChange}/>
+                        <label htmlFor='pets'>Pets</label>
+                    </div>
+                </div>
+                {/*end of extras*/}
+
+
+
             </form>
         </section>
     )
--- src/context.js
+++ src/context.js
@@ -71,6 +71,7 @@ class RoomProvider extends Component {
         let tempRooms=[...rooms]
         //transform the value
         capacity=parseInt(capacity)
+        price=parseInt(price)
         //filter by type
         if(type!=='all'){
             tempRooms=tempRooms.filter(room=>room.type===type)
@@ -79,6 +80,19 @@ class RoomProvider extends Component {
         if(capacity!==1){
             tempRooms=tempRooms.filter(room=>room.capacity>=capacity)
         }
+        //filter by price
+        tempRooms=tempRooms.filter(room=>room.price<=price)
+        //filter by size
+        tempRooms=tempRooms.filter(room=>room.size>=minSize&&room.size<=maxSize)
+        //filter by breakfast
+        if(breakfast){
+            tempRooms=tempRooms.filter(room=>room.breakfast===true)
+        }
+        //filter by pets
+        if(pets){
+            tempRooms=tempRooms.filter(room=>room.pets===true)
+        }
+        //state change
         this.setState({
             sortedRooms:tempRooms
         })
