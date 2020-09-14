0755cb46ce359a9afd25e7a0dc3379f53ed60bff
Rabbi50 <jasrabbi50@gmail.com>
2020-09-14 17:40:24 +0600

    create method for get unique value

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,12 +1,50 @@
 import React,{useContext} from 'react'
-import {RoomContext} from '../Context'
+import {RoomContext} from '../context'
 import Title from './Title'
 
-export default function RoomsFilter() {
+//get all unique value
+
+const getUnique=(items,value)=>{
+    return [...new Set(items.map(item=>item[value]))]
+}
+
+export default function RoomsFilter({rooms}) {
     const context =useContext(RoomContext)
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    }=context
+
+    //get unique types
+    let types=getUnique(rooms,'type')
+    //add all type
+    types=['all',...types]
+    //map to jsx
+    types=types.map((item,index)=>{
+    return<option value={item} key={index}>{item}</option>
+    })
     return (
-        <div>
-            Hello Froom room filter.
-        </div>
+        <section className='filter-container'>
+            <Title title='search rooms'/>
+            <form className='filter-form'>
+                {/*Select type*/}
+                <div className='form-group'>
+                    <label htmlFor='type'>room type</label>
+                    <select name='type' id='type' value={type} className='form-control' onChange={handleChange}>
+                        {types}
+                    </select>
+                </div>
+                {/*end Select type*/}
+
+            </form>
+        </section>
     )
 }
