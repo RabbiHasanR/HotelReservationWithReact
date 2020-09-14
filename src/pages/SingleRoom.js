a248bf4a97acb03766fe3acc376402fc2d712cb1
Rabbi50 <jasrabbi50@gmail.com>
2020-09-14 16:26:38 +0600

    finish singleroom page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -33,14 +33,50 @@ export default class SingleRoom extends Component {
             )
         }
         const {name,description,capacity,size,price,extras,breakfast,pets,images}=room
+        const [mainImg,...otherRoomImages]=images
         return (
-            <StyledHero img={images[0] || this.state.defaultImg}>
+            <>
+            <StyledHero img={mainImg || this.state.defaultImg}>
                 <Banner title={`${name} room`}>
                     <Link to='/rooms' className='btn-primary'>
                         back to rooms
                     </Link>
                 </Banner>
             </StyledHero>
+            <section className='single-room'>
+                <div className='single-room-images'>
+                    {otherRoomImages.map((item,index)=>{
+                       return <img key={index} src={item} alt={name}/>
+                    })}
+                </div>
+                <div className='single-room-info'>
+                    <article className='desc'>
+                        <h3>details</h3>
+                        <p>{description}</p>
+                    </article>
+                    <article className='info'>
+                        <h3>info</h3>
+                        <h6>price : ${price}</h6>
+                        <h6>size : {size} SQFT</h6>
+                        <h6>
+                            max capacity : {
+                                capacity>1?`${capacity} people`:`${capacity} person`
+                            }
+                        </h6>
+                        <h6>pets : {pets?"pets allowed":"no pets allowed"}</h6>
+                        <h6>{breakfast && "free breakfast included"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className='room-extras'>
+                <h6>extras</h6>
+                <ul className='extras'>
+                    {extras.map((item,index)=>{
+                        return <li key={index}>- {item}</li>
+                    })}
+                </ul>
+            </section>
+            </>
         )
     }
 }
