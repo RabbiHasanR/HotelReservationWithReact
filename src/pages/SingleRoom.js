10534ad776910509aff34a09b3225c232f54867a
Rabbi50 <jasrabbi50@gmail.com>
2020-09-15 18:13:28 +0600

    remove Hero component from SingleRoom component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import defaultImg from '../images/room-1.jpeg'
-import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {RoomContext} from '../context'
 import {Link} from 'react-router-dom'
